2489a840e4373368701b5ed745390fe0a5d716e2
UmangKolhe <umangkolhe25@gmail.com>
2023-02-20 16:26:57 +0530

    first change

--- umang.java
+++ umang.java
@@ -2,5 +2,6 @@ class Umang {
        
      publuc static void main(String[]args){
             System.out.println("umang developer");
+            System.out.println("change done by dev2 developer");
        }
     }
