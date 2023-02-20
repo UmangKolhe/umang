b278a581920c48ad97c94be137a685ba137c1a4b
UmangKolhe <umangkolhe25@gmail.com>
2023-02-20 16:51:19 +0530

    file merge

--- umang.java
+++ umang.java
@@ -3,5 +3,6 @@ class Umang {
      publuc static void main(String[]args){
             System.out.println("umang developer");
             System.out.println("change done by dev2 developer");
+            System.out.println("change  done by devmerge developer");
        }
     }
